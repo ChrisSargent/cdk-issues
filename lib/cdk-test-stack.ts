8dc8b0f90b94432d98e5f28a71b7f2284d8da397
Chris Sargent <chris@noah.com>
2021-08-16 10:57:37 +0200

    adding commits and branch for pipelines issue

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -1,14 +1,60 @@
 import * as cdk from "@aws-cdk/core";
+import * as pipelines from "@aws-cdk/pipelines";
+import * as codepipeline from "@aws-cdk/aws-codepipeline";
+import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
 import * as api from "@aws-cdk/aws-apigateway";
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const restApi = new api.RestApi(this, "test-api", {
-      endpointExportName: "EndpointTestName",
+    const cdkInput = pipelines.CodePipelineSource.gitHub(
+      "ChrisSargent/cdk-issues",
+      "pipelines"
+    );
+
+    // Setup the code source action
+    const sourceOutput = new codepipeline.Artifact();
+    const sourceAction = new codepipeline_actions.GitHubSourceAction({
+      owner: "ChrisSargent",
+      repo: "cdk-issues",
+      branch: "pipelines",
+      actionName: "SourceAction",
+      output: sourceOutput,
+      oauthToken: cdk.SecretValue.secretsManager("git/ChrisSargent"),
     });
 
-    restApi.root.addMethod("ANY");
+    const pipeline = new codepipeline.Pipeline(this, "Pipeline", {
+      stages: [
+        {
+          actions: [sourceAction],
+          stageName: "GitSource",
+        },
+      ],
+    });
+
+    const cdkPipeline = new pipelines.CodePipeline(this, "CDKPipeline", {
+      codePipeline: pipeline,
+      synth: new pipelines.ShellStep("Synth", {
+        // Without input, we get: Error: CodeBuild action 'Synth' requires an input (and the pipeline doesn't have a Source to fall back to). Add an input or a pipeline source.
+        // With input, we get:Error: Validation failed with the following errors: Source actions may only occur in first stage
+        input: cdkInput,
+        commands: ["yarn install --frozen-lockfile", "npx cdk synth"],
+      }),
+    });
+
+    // Produces: Stage 'PreProd' must have at least one action
+    // pipeline.addStage(new MyApplication(this, "PreProd"));
+
+    // Produces: The given Stage construct ('CdkTestStack/PreProd') should contain at least one Stack
+    cdkPipeline.addStage(new MyApplication(this, "PreProd"));
+  }
+}
+
+class MyApplication extends cdk.Stage {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StageProps) {
+    super(scope, id, props);
+
+    console.log("Nothing to deploy");
   }
 }
